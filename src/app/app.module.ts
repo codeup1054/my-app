61b8ef08e13bd9a18725af72b4f6843fa1ab1656
codeup1054 <codeup1054@gmail.com>
2021-08-02 21:09:00 +0300

    2021-08-02 product list update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from "./product-list/product-list.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
   ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
